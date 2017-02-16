57f4db8d316684fd18f0a5c39c4bb612fd424ce1
Neng Liangpornrattana <neng@taskworld.com>
2017-02-16 22:29:53 +0700

    Refactor the rest of exchange balance

--- src/main/java/com/exchange/app/Balance.java
+++ src/main/java/com/exchange/app/Balance.java
@@ -17,14 +17,9 @@ public class Balance
   private void exchange(int balance) {
     int accumulatedResult = this.exchangeBanknote(500, balance);
     accumulatedResult = this.exchangeBanknote(100, accumulatedResult);
-    int banknote50Amt = accumulatedResult / 50;
-    banknote.put(50, banknote50Amt);
-    accumulatedResult = accumulatedResult - (banknote50Amt * 50);
-    int banknote20Amt = accumulatedResult / 20;
-    banknote.put(20, banknote20Amt);
-    accumulatedResult = accumulatedResult - (banknote20Amt * 20);
-    int banknote10Amt = accumulatedResult / 10;
-    banknote.put(10, banknote10Amt);
+    accumulatedResult = this.exchangeBanknote(50, accumulatedResult);
+    accumulatedResult = this.exchangeBanknote(20, accumulatedResult);
+    accumulatedResult = this.exchangeBanknote(10, accumulatedResult);
   }
 
   private int exchangeBanknote(int value, int balance) {
